fdc4b7b45978ce3128119a3c8aeb2638e34b22f1
Pooja <103728019+Pooja2579@users.noreply.github.com>
2026-08-17 17:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (50.41%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        current_max = -1
-        res= 0
-        
+        current_max = 0
+        res = 0
 
-        for i, n in enumerate(arr):
-            current_max = max(n, current_max)
+        for i in range(len(arr)):
+            current_max = max(arr[i], current_max)
 
             if current_max == i:
-                res +=1
-
+                res += 1
         return res
